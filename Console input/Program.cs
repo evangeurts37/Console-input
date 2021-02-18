46075ba8699af75eb48da766ef0de377754990aa
Evan Geurts <GeurEvan189@GoTVDSB.ca>
2021-02-18 12:41:53 -0500

    Add project files.

---
+++ Console input/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+
+            DateTime years = DateTime.Today;
+
+
+
+
+            Console.WriteLine($"your name is {name} and you were born in {age} years old, the current year is {years.ToString("yyyy")}");
+
+
+            years = 
+            
+        }
+    }
+}
